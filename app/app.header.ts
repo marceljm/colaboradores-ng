f94bfa581c6243ddde8a184c031a74165ced6e37
marceljm <marceljmachado@gmail.com>
2016-11-09 15:59:08 -0200

    Header added

---
+++ app/app.header.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: 'app/app.header.html',
+    selector: 'header-app'
+})
+export class AppHeader {
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,15 @@ import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
 import 'rxjs/add/operator/toPromise';
 
+import {AppHeader} from './app.header';
 import {AppComponent}  from './app.component';
 import {CarService} from './cars/carservice';
 import {InputTextModule, DataTableModule, ButtonModule, DialogModule} from 'primeng/primeng';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpModule, InputTextModule, DataTableModule, ButtonModule, DialogModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  declarations: [AppHeader, AppComponent],
+  bootstrap: [AppHeader, AppComponent],
   providers: [CarService]
 })
 export class AppModule { }
