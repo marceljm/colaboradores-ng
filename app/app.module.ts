5ab0233f71db92b8222793fdda8be28292b3269e
marceljm <marceljmachado@gmail.com>
2016-11-09 17:52:24 -0200

    TabView added

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,14 +5,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import 'rxjs/add/operator/toPromise';
 
 import {AppHeader} from './app.header';
-import {AppComponent}  from './app.component';
+import {AppComponent} from './app.component';
+import {AppComponent2}  from './app.component2';
 import {CarService} from './cars/carservice';
-import {InputTextModule, DataTableModule, ButtonModule, DialogModule} from 'primeng/primeng';
+import {InputTextModule, DataTableModule, ButtonModule, DialogModule, TabViewModule} from 'primeng/primeng';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpModule, InputTextModule, DataTableModule, ButtonModule, DialogModule],
-  declarations: [AppHeader, AppComponent],
-  bootstrap: [AppHeader, AppComponent],
+  imports: [BrowserModule, FormsModule, HttpModule, InputTextModule, DataTableModule, ButtonModule, DialogModule, TabViewModule],
+  declarations: [AppHeader, AppComponent, AppComponent2],
+  bootstrap: [AppHeader, AppComponent, AppComponent2],
   providers: [CarService]
 })
 export class AppModule { }
