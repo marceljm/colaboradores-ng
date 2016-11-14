4378d30b16f659b5498e25917b38e5f443e5cc2c
marceljm <marceljmachado@gmail.com>
2016-11-14 16:23:10 -0200

    First chart ready

--- app/app.home.ts
+++ app/app.home.ts
@@ -8,12 +8,20 @@ import {VwColabSituacaoQtdeOperService} from './charts/vwColabSituacaoQtdeOperSe
 })
 export class AppHome implements OnInit {
     errorMessage: string;
+    colorList: string[] = [
+        "#4BC0C0",
+        "#36A2EB",
+        "#FF6384",
+        "#FFCE56",
+        "#E7E9ED"
+    ]
 
     vwColabSituacaoQtdeOper: VwColabSituacaoQtdeOper = new VwColabSituacaoQtdeOper();
     vwColabSituacaoQtdeOperChart: any;
 
     vwColabSituacaoQtdeOperList: VwColabSituacaoQtdeOper[];
-    vwColabSituacaoQtdeOperLabel: string[]; 
+    vwColabSituacaoQtdeOperLabel: Array<string> = new Array<string>();
+    vwColabSituacaoQtdeOperAmount: Array<number> = new Array<number>();
 
     constructor(
         private vwColabSituacaoQtdeOperService: VwColabSituacaoQtdeOperService) { }
@@ -27,26 +35,25 @@ export class AppHome implements OnInit {
         this.vwColabSituacaoQtdeOperService.getVwColabSituacaoQtdeOper().subscribe(
             vwColabSituacaoQtdeOperList => this.vwColabSituacaoQtdeOperList = vwColabSituacaoQtdeOperList,
             error => this.errorMessage = <any>error,
-            () => console.log(this.vwColabSituacaoQtdeOperList)            
+            () => {
+                for (let entry of this.vwColabSituacaoQtdeOperList) {
+                    this.vwColabSituacaoQtdeOperAmount.push(entry.quantidade);
+                    this.vwColabSituacaoQtdeOperLabel.push(entry.situacao);
+                }
+                console.log(this.vwColabSituacaoQtdeOperLabel);
+                this.showVwColabSituacaoQtdeOperChart();
+            }
         );
     }
 
     showVwColabSituacaoQtdeOperChart() {
         this.vwColabSituacaoQtdeOperChart = {
-            labels: ['A', 'B', 'C'],
+            labels: this.vwColabSituacaoQtdeOperLabel,
             datasets: [
                 {
-                    data: [300, 50, 100],
-                    backgroundColor: [
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56"
-                    ],
-                    hoverBackgroundColor: [
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56"
-                    ]
+                    data: this.vwColabSituacaoQtdeOperAmount,
+                    backgroundColor: this.colorList,
+                    hoverBackgroundColor: this.colorList
                 }]
         }
     }
