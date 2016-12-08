e15e501dda44eeabec5de5d30ccb8711398503d5
marceljm <marceljmachado@gmail.com>
2016-12-08 17:19:53 -0200

    Validation test implemented

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import 'rxjs/add/operator/toPromise';
@@ -10,15 +10,16 @@ import { AppHome } from './app.home';
 import { AppTree } from './app.tree';
 import { AppComponent } from './app.component';
 import { AppCar } from './app.car';
+import { ValidationDemo } from './ValidationDemo';
 
 import { CarService } from './cars/carservice';
 
 import { InputTextModule, DataTableModule, ButtonModule, DialogModule, TabViewModule, ChartModule, TreeModule, GrowlModule, InputSwitchModule, BlockUIModule, InputMaskModule } from 'primeng/primeng';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpModule, InputTextModule, DataTableModule, ButtonModule, DialogModule, TabViewModule, ChartModule, TreeModule, GrowlModule, InputSwitchModule, BlockUIModule, InputMaskModule],
-    declarations: [AppHeader, AppComponent, AppHome, AppTree, AppConfig, AppCar],
-    bootstrap: [AppHeader, AppComponent, AppHome, AppTree, AppConfig, AppCar],
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpModule, InputTextModule, DataTableModule, ButtonModule, DialogModule, TabViewModule, ChartModule, TreeModule, GrowlModule, InputSwitchModule, BlockUIModule, InputMaskModule],
+    declarations: [AppHeader, AppComponent, AppHome, AppTree, AppConfig, AppCar, ValidationDemo],
+    bootstrap: [AppHeader, AppComponent, AppHome, AppTree, AppConfig, AppCar, ValidationDemo],
     providers: [CarService]
 })
 export class AppModule { }
